0baf7ae1b4ae817d2b7e1336600cfd1992cbd33f
Djatiaja <djamgt23@gmail.com>
2026-04-02 09:59:22 +0700

    feat(favicon): change logo culfest

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,9 @@ const merriweather = Merriweather({
 export const metadata: Metadata = {
   title: "Culfest Web 2025",
   description: "Culfest Web 2025",
+  icons: {
+    icon: "/images/logo-culfest.png",
+  },
 };
 
 export default function RootLayout({
